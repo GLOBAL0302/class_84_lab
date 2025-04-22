a944c05845272615831cba4601c2941c720cce22
kubanych030299@gmail.com <kubanych030299@gmail.com>
2025-04-22 01:42:20 -0400

    git init

---
+++ back/index.ts
@@ -0,0 +1,24 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import config from './config';
+
+const app = express();
+const port = 8000;
+app.use(express.json());
+app.use(express.static('public'));
+
+const run = async () => {
+  await mongoose.connect(config.db);
+
+  app.listen(port, () => {
+    console.log(`Server is running on port http://localhost:${port}`);
+  });
+
+  process.on('exit', () => {
+    mongoose.disconnect();
+  });
+};
+
+run().catch((e) => {
+  console.error(e);
+});
